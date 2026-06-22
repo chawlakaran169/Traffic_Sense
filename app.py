962240955e3179f1d30816be23f0be1eb65fc295
stock_holmes <karanchayal2003@gmail.com>
2026-06-22 19:51:53 +0530

    Fix GitHub Pages dashboard entry

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('traffic_dashboard'))
 
 
 @app.route('/dashboard')
